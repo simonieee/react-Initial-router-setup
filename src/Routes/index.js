1cd023840021f179d7c256adfa35ad3660afb33d
simonieee <75366051+simonieee@users.noreply.github.com>
2023-05-02 15:15:16 +0900

    [UPDATE] z

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Main } from "./Pages";
+import { MainLayout } from "../Layout";
 
 const index = () => {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Main />} />
+      </Route>
     </Routes>
   );
 };
